b9fbd8c9000595cfc6710d06ce7f68a1c358c040
jessica <hsujessica21@gmail.com>
2017-10-04 21:24:14 -0500

    update README, update files

--- project1/README.md
+++ project1/README.md
@@ -1,2 +1,8 @@
 # Full-stack-Nanodegree
-projects from full-stack nanodegree
+Project 1: Movie Trailer Website
+
+Verify that you have python installed <br>
+Download entertainment_center.py, media.py, and fresh_tomatoes.py <br>
+Open terminal/command line <br>
+Go to directory where you stored the three files <br>
+Run with: 'python entertainment_center.py'
--- project1/media.py
+++ project1/media.py
@@ -1,6 +1,7 @@
 import webbrowser
 
 class Movie():
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
@@ -9,4 +10,3 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
-        
