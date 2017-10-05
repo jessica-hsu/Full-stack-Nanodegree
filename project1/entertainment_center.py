02ef9a5f8d72e4cf325d11b5885ccab7230fe9f8
jessica <hsujessica21@gmail.com>
2017-10-04 19:05:13 -0500

    add movie instances to file

--- project1/entertainment_center.py
+++ project1/entertainment_center.py
@@ -1,11 +1,12 @@
 import media
 
-toy_story = media.Movie("Toy Story", "A story about toys coming to life", "image", "trailerLink")
 
-print(toy_story.storyline)
+train_to_busan = media.Movie("Train To Busan", "While a zombie virus breaks out in South Korea, passengers struggle to survive on the train from Seoul to Busan.","https://en.wikipedia.org/wiki/Train_to_Busan#/media/File:Train_to_Busan.jpg","https://www.youtube.com/watch?v=pyWuHv2-Abk")
+the_imitation_game = media.Movie("The Imitation Game", "During World War II, mathematician Alan Turing tries to crack the enigma code with help from fellow mathematicians.","https://en.wikipedia.org/wiki/The_Imitation_Game#/media/File:The_Imitation_Game_poster.jpg","https://www.youtube.com/watch?v=S5CjKEFb-sM")
+rogue_one = media.Movie("Rogue One", "The Rebel Alliance makes a risky move to steal the plans for the Death Star, setting up the epic saga to follow.","https://en.wikipedia.org/wiki/Rogue_One#/media/File:Rogue_One,_A_Star_Wars_Story_poster.png", "https://www.youtube.com/watch?v=frdj1zb9sMY")
+v_for_vendetta = media.Movie("V For Vendetta", "In a future British tyranny, a shadowy freedom fighter, known only by the alias of "V", plots to overthrow it with the help of a young woman.","https://en.wikipedia.org/wiki/V_for_Vendetta_(film)#/media/File:Vforvendettamov.jpg", "https://www.youtube.com/watch?v=qxyUl9M_7vc")
+zombieland = media.Movie("Zombieland", "A shy student trying to reach his family in Ohio, a gun-toting tough guy trying to find the last Twinkie, and a pair of sisters trying to get to an amusement park join forces to travel across a zombie-filled America.","https://en.wikipedia.org/wiki/Zombieland#/media/File:Zombieland-poster.jpg", "https://www.youtube.com/watch?v=8m9EVP8X7N8")
+our_times = media.Movie("Our Times", "Love grows where it isn't expected in this endearing romantic comedy coming of age movie.", "https://upload.wikimedia.org/wikipedia/en/f/f3/Our_Times%2C_Movie_Poster.jpg","https://www.youtube.com/watch?v=ER61b0ejzlg")
 
-avatar = media.Movie("Avatar", "a marine on an alien planet", "avatar pic", "ahttps://www.youtube.com/watch?v=5PSNL1qE6VY")
-
-print(avatar.storyline)
-
-avatar.show_trailer()
+#print(train_to_busan.storyline)
+#train_to_busan.show_trailer()
