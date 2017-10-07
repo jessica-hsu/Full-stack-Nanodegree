d04d516f7b37da7813cac07d6a2e9682f7393f87
jessica <hsujessica21@gmail.com>
2017-10-06 23:39:45 -0500

    [media.py] fix formatting

--- project1/media.py
+++ project1/media.py
@@ -1,9 +1,20 @@
 import webbrowser
 
+
+'''
+This file contains the constructor __init__ that creates a Movie object
+that represents a movie. It holds information such as title,
+storyline, poster url, and trailer url.
+It includes one method, show_trailer(), that opens a
+browser using the given trailer url.
+'''
+
+
 class Movie():
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
