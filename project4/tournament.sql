2184d39d14ce8bd04e8bafae184ea1f2eb84a52a
jessica <hsujessica21@gmail.com>
2018-01-19 19:43:05 -0600

    [tournament.sql] fix syntax error

--- project4/tournament.sql
+++ project4/tournament.sql
@@ -4,17 +4,17 @@ CREATE DATABASE tournament;
 
 -- Drop/Create players table w/columns (id, name)
 DROP TABLE IF EXISTS players CASCADE; --CASCADE needed to remove dependency
-CREATE TABLE players {
+CREATE TABLE players (
 	id serial not null,
 	full_name varchar(30) not null,
 	wins int,
 	matches int,
 	PRIMARY KEY(id)
-};
+);
 
 -- Drop/Create matches table w/columns (winner id, loser id)
 DROP TABLE IF EXISTS matches;
-CREATE TABLE matches {
+CREATE TABLE matches (
 	winner_id serial references players(id) not null,
 	loser_id serial references players(id) not null
-};
+);
