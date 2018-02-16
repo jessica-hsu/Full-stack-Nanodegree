7f8b769abba3125a01b838e1a584e86f4a4a2f69
jessica <hsujessica21@gmail.com>
2018-02-16 17:37:25 -0600

    [application.py, index.html] continue code for rendering pages

--- project5/application.py
+++ project5/application.py
@@ -34,20 +34,34 @@ def load_main_page():
 
 # View items in selected category
 # valid URL for viewing items of a category
-# @app.route('/category/<int:category_id>')
-#
-# # Add new category
-# # valid URL for accessing add category page
-# @app.route('/category/add')
-#
-# # View categories to delete
-# # valid URL for viewing categories to delete
-# @app.route('/category/delete')
-#
-# # Delete category
-# # valid URL to actually delete category from database
-# @app.route('/category/delete/confirm')
-#
+@app.route('/category/<int:category_id>')
+def view_category_items(category_id):
+	# query to retrieve all items under selected category
+	items = session.query(Item).filter_by(category_id=category_id).all()
+	return render_template('show-items.html',categories=all_categories,items=items)
+
+# Add new category
+# valid URL for accessing add category page
+@app.route('/category/add', methods=['GET', 'POST'])
+def add_category():
+	new_name = requset.form['category-name']	# get new category name from form
+	if (request.method == 'POST'):	# post to database when user clicks submit
+		new_category = Category(name=new_name)
+		session.add(new_category)
+		session.commit()
+	return redirect('/')	# go back to home page to see new category appear
+
+# View categories to delete
+# valid URL for viewing categories to delete
+@app.route('/category/delete')
+def view_categories_to_delete():
+	return render_template('delete-category.html', categories=all_categories)
+
+# Delete category
+# valid URL to actually delete category from database
+@app.route('/category/delete/confirm')
+
+
 # # Add new item
 # # valid URL to add items
 # @app.route('/category/<int:category_id>/add')
