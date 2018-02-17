2033a4cde04d4d101af6154870aadd6fa5d75261
jessica <hsujessica21@gmail.com>
2018-02-17 17:27:14 -0600

    [application.py] fix bug for loading pages with POST requets

--- project5/application.py
+++ project5/application.py
@@ -41,20 +41,23 @@ def load_main_page():
 # valid URL for viewing items of a category
 @app.route('/category/<category_id>')
 def view_category_items(category_id):
+	current_category = session.query(Category).filter_by(id=category_id)
 	# query to retrieve all items under selected category
 	items = session.query(Item).filter_by(category_id=category_id)
-	return render_template('show-items.html',categories=all_categories,items=items)
+	return render_template('show-items.html',categories=all_categories,items=itemsm, category=current_category)
 
 # Add new category
 # valid URL for accessing add category page
 @app.route('/category/add', methods=['GET', 'POST'])
 def add_category():
-	new_name = requset.form['category-name']	# get new category name from form
 	if (request.method == 'POST'):	# post to database when user clicks submit
+		new_name = request.form['category-name']	# get new category name from form
 		new_category = Category(name=new_name)
 		session.add(new_category)
 		session.commit()
-	return redirect('/')	# go back to home page to see new category appear
+		return redirect(url_for('load_main_page'))
+	else:
+		return render_template('add-category.html', categories=all_categories)
 
 # View categories to delete
 # valid URL for viewing categories to delete
@@ -79,29 +82,33 @@ def delete_category_now(category_id):
 @app.route('/category/<category_id>/add', methods=['GET', 'POST'])
 def add_item(category_id):
 	category = session.query(Category).filter_by(id=category_id)
-	new_item_name = request.form['item-name']
-	new_item_description = request.form['item-description']
 	if (request.method == 'POST'):
+		new_item_name = request.form['item-name']
+		new_item_description = request.form['item-description']
 		new_item = Item(name=new_item_name, description=new_item_description, category_id=category_id)
 		session.add(new_item)
 		session.commit()
-	# redirect to see all items in selected category
-	redirect(url_for('view_category_items', category_id=category_id, category_name=category.name))
+		# redirect to see all items in selected category
+		redirect(url_for('view_category_items', category_id=category_id, category_name=category.name))
+	else:
+		render_template('add-item.html', category=category)
 
 # Edit item
 # valid URL to edit item
 @app.route('/category/<category_id>/<item_id>/edit', methods=['GET', 'POST'])
 def edit_item(category_id, item_id):
 	item_to_edit = session.query(Item).filter_by(id=item_id)
-	new_name = request.form['item-name']
-	new_description = request.form['item-description']
 	if (request.method == 'POST'):
+		new_name = request.form['item-name']
+		new_description = request.form['item-description']
 		item_to_edit.name = new_name
 		item_to_edit.description = new_item_description
 		session.add(item_to_edit)
 		session.commit()
-	# redirect to see all items in selected category
-	redirect(url_for('view_category_items', category_id=category_id))
+		# redirect to see all items in selected category
+		redirect(url_for('view_category_items', category_id=category_id))
+	else:
+		render_template('edit-item.html', item=item_to_edit)
 
 # Delete item
 # valid url to delete item from db
